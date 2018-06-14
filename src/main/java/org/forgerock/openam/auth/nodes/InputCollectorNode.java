15ad50eab323b6ded3140a8e6ad456d1940acbe0
Chandra Dhulipala <chandra.dhulipala@amido.com>
2018-06-14 16:10:49 +0100

    Updated the version and comments

--- src/main/java/org/forgerock/openam/auth/nodes/InputCollectorNode.java
+++ src/main/java/org/forgerock/openam/auth/nodes/InputCollectorNode.java
@@ -1,26 +1,3 @@
-/*
- * jon.knight@forgerock.com
- *
- * Sets user profile attributes 
- *
- */
-
-/*
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
- *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
- *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information: "Portions copyright [year] [name of copyright owner]".
- *
- * Copyright 2017 ForgeRock AS.
- */
-
 package org.forgerock.openam.auth.nodes;
 
 import com.google.inject.assistedinject.Assisted;
@@ -41,9 +18,9 @@ import java.util.*;
 import static org.forgerock.openam.auth.node.api.Action.send;
 
 /**
- * A node which collects a username from the user via a name callback.
+ * A node which collects a username/email from the user via a name callback.
  *
- * <p>Places the result in the shared state as 'username'.</p>
+ * <p>Places the result in the shared state as specified in the config.</p>
  */
 @Node.Metadata(outcomeProvider = SingleOutcomeNode.OutcomeProvider.class,
             configClass = InputCollectorNode.Config.class)
@@ -54,7 +31,6 @@ public class InputCollectorNode extends SingleOutcomeNode {
         Map<String, String> properties();
     }
 
-    private static final String BUNDLE = "org/forgerock/openam/auth/nodes/InputCollectorNode";
     private final static String DEBUG_FILE = "InputCollectorNode";
     protected Debug debug = Debug.getInstance(DEBUG_FILE);
 
